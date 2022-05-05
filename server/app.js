31cbe6ffff67ff682459bf12e1978dd932c9596b
Ahsan Shaikh <55804800+Ahsan2001@users.noreply.github.com>
2022-05-05 23:51:01 +0500

    update files

--- server/app.js
+++ server/app.js
@@ -1,22 +1,22 @@
 const express = require("express");
 const app = express();
+//bodyAllow
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 
+
+// security intailizing 
 const dotenv = require("dotenv");
 dotenv.config( { path: './config.env'} );
 
 
 
+// get database from mongoDB Server 
 require("./database/connection")
 const PORT = process.env.PORT;
 
 
-
-
-// app.use((req,res, next) => {
-//     console.log("running middleware Successfully")
-//     next();
-// })
 
 
 const middleware = (req,res, next) => {
@@ -29,40 +29,24 @@ const middleware = (req,res, next) => {
 
 
 
-app.get ('/' , (req, res ) =>{
-    res.send ("home page testing")
-});
-
-
-// app.get ('/about' , (req, res ) =>{
-//     res.send ("about page testing")
-//     console.log("running about testing")
-// });
-
 
 app.get ('/about' , middleware, (req, res ) =>{
     res.send ("about page testing")
     console.log("running about testing")
 });
 
-
-
 app.get ('/contact', (req, res ) =>{
     res.send ("contact page testing")
 });
-
 
 
 app.get ('/signin' , (req, res ) =>{
     res.send ("signin page testing")
 });
 
-
-
 app.get ('/signup' , (req, res ) =>{
     res.send ("signup page testing")
 });
-
 
 
 app.listen(PORT, () =>
