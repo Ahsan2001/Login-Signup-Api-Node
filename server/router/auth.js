eff9f8a6e84aee7653d332e7701b3bc9686ac2e6
Ahsan Shaikh <55804800+Ahsan2001@users.noreply.github.com>
2022-05-08 16:03:41 +0500

    update auth

--- server/router/auth.js
+++ server/router/auth.js
@@ -1,17 +1,21 @@
 const express = require("express");
-
 const router = express.Router();
 
-const middleware = (req,res, next) => {
-    console.log("running middleware Successfully")
-    next();
-}
+
+// const middleware = (req,res, next) => {
+//     console.log("running middleware Successfully")
+//     next();
+// }
+
+
+
+
 
 router.get ('/' , (req, res ) =>{
     res.send ("home page testing router ")
 });
 
-router.get ('/about', middleware,(req, res ) =>{
+router.get ('/about',(req, res ) =>{
     res.send ("about page testing")
     console.log("running about testing")
 });
@@ -28,11 +32,8 @@ router.post ('/register' , (req, res ) =>{
 
     const {name,email,phone,work,password,confirmPassword} = req.body;
     if ( !name  || !email || !phone || !work || !password || !confirmPassword ) {
-        res.json({error: "please fill the field properly"})
+        res.status(422).json({error: "please fill the field properly"})
     }
-
-    console.log(req.body)
-    res.json({ message: req.body});
 
 
 });
