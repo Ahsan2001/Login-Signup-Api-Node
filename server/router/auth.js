5191de384aa7aac1d98f0c86f442b030d9c788c4
Ahsan2001 <ahsan2sabir@gmail.com>
2022-05-13 05:14:16 +0500

    updated files

--- server/router/auth.js
+++ server/router/auth.js
@@ -1,17 +1,18 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
 
-
 const router = express.Router();
 
 require("../database/connection")
-const User = require("../module/userSchema")
+
+const User = require("../module/userSchema");
+
+
 
 // const middleware = (req,res, next) => {
 //     console.log("running middleware Successfully")
 //     next();
 // }
-
 
 router.get('/', (req, res) => {
     res.send("home page testing router ")
@@ -32,7 +33,6 @@ router.get('/login', (req, res) => {
 });
 
 
-
 // thappa bhai hai yeh 
 
 router.post('/register', (req, res) => {
@@ -46,19 +46,15 @@ router.post('/register', (req, res) => {
                 return res.status(422).json({ error: "User Already Exits" })
             }
             const user = new User({ name, email, phone, work, password, confirmPassword })
-            user.save().then(() => {
-                res.status(201).json({ message: "User Successfully Registered" })
-            })
+            user.save()
+                .then(() => {
+                    res.status(201).json({ message: "User Successfully Registered" })
+                })
                 .catch((err) => {
                     res.status(500).json({ error: "Not Registered" })
                 })
         })
 });
-
-
-
-
-
 
 // sir jaffer hai yeh walay 
 
@@ -72,7 +68,7 @@ router.post("/api/signup", async (req, res) => {
         ...req.body,
         password: hashPass,
     };
-    User.findOne({ email}, (error, user) => {
+    User.findOne({ email }, (error, user) => {
         if (error) {
             res.send(error);
         } else if (user) {
@@ -89,8 +85,6 @@ router.post("/api/signup", async (req, res) => {
         }
     })
 });
-
-
 
 router.post("/api/login", (req, res) => {
     const { email, password } = req.body;
@@ -123,23 +117,67 @@ router.post("/api/login", (req, res) => {
 
 
 
-
-
-
-
-
 // hamary pangay hai yeha say ab 
+router.post("/api/v2/signup", async (req, res) => {
+    const { name, phone, email, password, confirmPassword, work } = req.body;
 
+    if (!name, !phone, !email, !password, !confirmPassword, !work) {
+        res.send({ message: "please Put All the Fields" })
+    }
+    const passHashKarhaHoon = await bcrypt.hash(password,10)
+    const userObj2 = {
+        ...req.body,
+        password : passHashKarhaHoon,
 
+    };
+    User.findOne({ email: email }, (error, user) => {
+        if (error) {
+            res.send(error)
+        }
+        else if (user) {
+            res.send({ message: "email is already registered" })
+        } else {
+            User.create(userObj2, (err, _) => {
+                if (err) {
+                    res.send(err)
+                }
+                else {
+                    res.send({ message: "hogya ja moj kar" })
+                }
+            })
+        }
+    })
+}
+)
 
+router.post("/api/v2/login", (req, res) => {
+    const { email, password } = req.body;
 
+    if (!email, !password) {
+        res.send({ message: "Enter tu karo jani kuch" })
+    }
 
-
-
-
-
-
-
-
+    User.findOne({ email: email }, async (error, user) => {
+        if (error) {
+            res.send(error)
+        }
+        else if (user) {
+           await bcrypt.compare(password, user.password)
+           .then( (password)=> {
+                if(password) {
+                    res.send({message: "password sahi hai congratz haha !"})
+                }
+                else  {
+                    res.send({ message: "password sahi nhi hai sed loif ! "})
+                }
+           } ) 
+           .catch((err) => { res.send(err) });
+        } 
+        else {
+            res.send({ message: "jani login credentials sahi dalo na esay kesa hoga login" })
+        }
+    })
+}
+)
 
 module.exports = router;
